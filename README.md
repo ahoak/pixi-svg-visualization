f1cbd9918a87d1d04665abbd3d8e8d98ba72b0b6
Amber Hoak <amhoak@microsoft.com>
2020-07-22 08:25:52 -0700

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Pixi-SVG-Visualization
-
-Example Simple Data Visual using Pixi.js webGL renderer layered on top of SVG.
-
-deployed application: https://ahoak.github.io/pixi-svg-visualization
-
-Icons made by Freepik from www.flaticon.com
---
+++ precache-manifest.b384eecd0200c8b61dd3a900fa43e07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4430e881800bd3fa751ffdc4625ee978",
+    "url": "/pixi-svg-visualization/index.html"
+  },
+  {
+    "revision": "1dd57af9d5d1d999b727",
+    "url": "/pixi-svg-visualization/static/css/main.fde7c068.chunk.css"
+  },
+  {
+    "revision": "6caba3245db9a05d00f5",
+    "url": "/pixi-svg-visualization/static/js/2.dbd1a682.chunk.js"
+  },
+  {
+    "revision": "ffb64eb2a52f8276335ad8f8c7a5ce9b",
+    "url": "/pixi-svg-visualization/static/js/2.dbd1a682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd57af9d5d1d999b727",
+    "url": "/pixi-svg-visualization/static/js/main.d6be97e6.chunk.js"
+  },
+  {
+    "revision": "701de8c3211785045759",
+    "url": "/pixi-svg-visualization/static/js/runtime-main.688227c4.js"
+  }
+]);
